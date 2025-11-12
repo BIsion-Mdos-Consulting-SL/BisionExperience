f2b52719c6a0232cf11e6c68a31bd5935209646d
AaronGSanti <matiasmora@bi-sion.es>
2025-11-12 13:38:53 +0100

    editar_restaurante_imagen

--- resources/views/cliente/modals/modal_info_restaurante.blade.php
+++ resources/views/cliente/modals/modal_info_restaurante.blade.php
@@ -2,25 +2,36 @@
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content p-4">
             @if(isset($restaurante))
-            <div class="modal-header">
-                <h1 class="fw-bold" style="color: black; font-size: xx-large;">{{$restaurante->nombre}}</h1>
+            <div class="modal-header border-0 pb-0">
+                <h1 class="fw-bold m-0" style="color: black; font-size: xx-large;">
+                    {{ $restaurante->nombre }}
+                </h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body d-flex flex-wrap gap-5">
 
-                <div>
-                    <img src="{{ Storage::url($restaurante->foto_restaurante) }}" class="mt-2 w-60">
+            <hr class="mt-2 mb-4" style="border-top: 2px solid #e0e0e0;">
+
+            <div class="modal-body pt-0 d-flex flex-column gap-4">
+                <div class="p-2">
+                    <img src="{{ Storage::url($restaurante->foto_restaurante) }}"
+                        class="img-fluid w-100 rounded" style="height: 350px;"
+                        alt="Foto del restaurante">
                 </div>
-                <div>
-                    <p style="color: black; font-style: italic; font-size: medium;">{{$restaurante->descripcion}}</p>
-                </div>
-                <div class="d-flex ms-auto">
-                    <a href="{{ $restaurante->enlace }}"
-                        class="p-2 rounded d-inline-flex align-items-center gap-2 ms-auto"
-                        style="background-color:#05072e; color:#fff;">
-                        <i class="bi bi-geo-alt-fill"></i>
-                        <span>Ver mapa</span>
-                    </a>
+
+                <div class="mx-auto w-100">
+                    <p class="mb-3"
+                        style="color:black; font-style:italic; font-size:medium; text-align:justify;">
+                        {{ $restaurante->descripcion }}
+                    </p>
+
+                    <div class="d-flex justify-content-end">
+                        <a href="{{ $restaurante->enlace }}"
+                            class="p-2 rounded d-inline-flex align-items-center gap-2"
+                            style="background-color:#05072e; color:#fff;">
+                            <i class="bi bi-geo-alt-fill"></i>
+                            <span>Ver mapa</span>
+                        </a>
+                    </div>
                 </div>
             </div>
             @endif
